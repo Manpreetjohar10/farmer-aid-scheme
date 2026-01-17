af0a351b07e99f5cfc7c636107e160ef3836336d
manpreet <152253313+Manpreetjohar10@users.noreply.github.com>
2026-01-17 19:52:58 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCoeAxQ2QCOJx1_WS8Pt1FdrHxEEgpP86g",
-  authDomain: "farmer-aid-scheme.firebaseapp.com",
-  projectId: "farmer-aid-scheme",
-  storageBucket: "farmer-aid-scheme.appspot.com",
-  messagingSenderId: "137270676424",
-  appId: "1:137270676424:web:1ea3c446cf6964eca2cefd"
+  apiKey: "###########",
+  authDomain: "########",
+  projectId: "#######",
+  storageBucket: "########",
+  messagingSenderId: "######",
+  appId: ########
 };
 
 const app = initializeApp(firebaseConfig);
